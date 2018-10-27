1251a08bb3816fc440ea0b8dde90b501d25728d9
Mash <mbedra666@gmail.com>
2018-10-27 16:39:03 +0200

    Replace invalid log message in AppService (fix for new logging library)

--- filenames/src/main/java/net/bedra/maciej/filenames/services/AppService.java
+++ filenames/src/main/java/net/bedra/maciej/filenames/services/AppService.java
@@ -1,7 +1,6 @@
 package net.bedra.maciej.filenames.services;
 
 import java.io.File;
-import java.util.regex.Matcher;
 import net.bedra.maciej.mblogging.Logger;
 import org.springframework.stereotype.Service;
 
@@ -22,8 +21,7 @@ public class AppService {
 	 * @return boolean logical value if given path is directory
 	 */
 	public boolean isDirectory(String path) {
-		String logPath = path.replaceAll(Matcher.quoteReplacement(File.separator), "/");
-		log.info("Checking if path [{}] is directory...", !logPath.isEmpty() ? logPath : "null");
+		log.info("Checking if path [{}] is directory...", !path.isEmpty() ? path : null);
 		boolean isDir = new File(path).isDirectory();
 		log.info("Path checked [is directory = {}]", isDir);
 
