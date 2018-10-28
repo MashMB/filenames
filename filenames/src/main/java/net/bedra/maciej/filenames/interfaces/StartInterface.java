2290b052b10a5751833de42717aa02798669a3ec
Mash <mbedra666@gmail.com>
2018-10-28 18:21:20 +0100

    Add interfaces for available scan and start operations

---
+++ filenames/src/main/java/net/bedra/maciej/filenames/interfaces/StartInterface.java
@@ -0,0 +1,15 @@
+package net.bedra.maciej.filenames.interfaces;
+
+/**
+ * Interface for all kind renaming operations.
+ *
+ * @author Maciej Bedra
+ */
+public interface StartInterface {
+
+	/**
+	 * Perform renaming operation for all kind of conditions.
+	 */
+	void startRenaming();
+
+}
