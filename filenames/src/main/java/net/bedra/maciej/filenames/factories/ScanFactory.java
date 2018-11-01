5cff121c9fa8665cb5a4ee30eec09377c13efedf
Mash <mbedra666@gmail.com>
2018-11-01 14:12:26 +0100

    Add factory to return proper scan implementation (currently: all names, all files strategy)

---
+++ filenames/src/main/java/net/bedra/maciej/filenames/factories/ScanFactory.java
@@ -0,0 +1,44 @@
+package net.bedra.maciej.filenames.factories;
+
+import javafx.scene.control.TextArea;
+import net.bedra.maciej.filenames.implementations.AllAllScanImpl;
+import net.bedra.maciej.filenames.interfaces.ScanInterface;
+
+/**
+ * Factory to ensure proper scan implementation.
+ *
+ * @author Maciej Bedra
+ */
+public class ScanFactory {
+
+	private String directory;
+	private TextArea userLogArea;
+
+	/**
+	 * ScanFactory constructor.
+	 *
+	 * @param directory   path to directory
+	 * @param userLogArea visible user logs
+	 */
+	public ScanFactory(String directory, TextArea userLogArea) {
+		this.directory = directory;
+		this.userLogArea = userLogArea;
+	}
+
+	/**
+	 * Get proper scan implementation that depends on chosen mode.
+	 *
+	 * @param mode chosen scan mode
+	 * @return ScanInterface scan implementation for chosen scan mode
+	 */
+	public ScanInterface getImplementation(String mode) {
+		switch (mode) {
+			case "AA":
+				return new AllAllScanImpl(directory, userLogArea);
+
+			default:
+				throw new RuntimeException("Implementation of ScanInterface not found (mode = " + mode + ")");
+		}
+	}
+
+}
