3192fced975eea0b91a2092271f5bf1f919c6553
Mash <mbedra666@gmail.com>
2018-11-07 20:59:08 +0100

    Refactor name of rename opeartions interface

---
+++ filenames/src/main/java/net/bedra/maciej/filenames/interfaces/RenameInterface.java
@@ -0,0 +1,15 @@
+package net.bedra.maciej.filenames.interfaces;
+
+/**
+ * Interface for all kind of renaming operations.
+ *
+ * @author Maciej Bedra
+ */
+public interface RenameInterface {
+
+	/**
+	 * Perform rename operation for all kind of conditions.
+	 */
+	void startRenaming();
+
+}
